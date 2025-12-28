19cf2d328634a67bf3efd38a49325dfa5b529697
nickname-1725 <nickname-1725@outlook.com>
2025-12-28 23:33:47 +0800

    增加 random-select-N-from-list 用于从列表中不重复地取出 N 个成员；增加 make-board-row/5，用于生成网格的一行，每个元素自动获取其相邻节点；增加 make-board/6 作为 make-board/3 的帮助函数；现在可以使用 make-board/3 生成每个节点自动获取 3x3 九宫格的网格；预计使用 random-select-N-from-list/3 实现雷块的选取和赋雷

--- main.pl
+++ main.pl
@@ -1,34 +1,69 @@
 
-'make-board'(Height,Width,X) :-
-  % 创建出 Height 行，Width 列的网格
-  Height =:= 0, !, X = [];
-  !, length(List, Width), X = [List|Rest],
+'random-select-N-from-list'(N, From, Result) :-
+  % 'random-select-N-from-list'(+N, +From, -Result)
+  % 从 From 列表中随机抽取 N 个不重复元素，组成 Result 列表
+  From = [],!, Result = [];
+  N =:= 0, !, Result = [];
+  !, Result = [X|Result_],
+  N_ is N - 1,
+  random_select(X,From,From_),
+  'random-select-N-from-list'(N_, From_, Result_).
+
+% O|--- X
+%  |
+%  |Y
+
+'make-board-row'(Y, X, Width, Result, This) :-
+  % 'make-board-row'(+Y, +X, +Width, -Result, +This)
+  % 创建出网格中的一行
+  % 每个节点自动获取 3x3 范围内的相邻节点
+  Width =:= 0,!, Result=[];
+  !, 
+  'get-3x3'(Y,X,This,Neighbor),
+  Result = ['block'('open?'(_),'number'(_),'neighbor'(Neighbor))|Rest],
+  Width_ is Width - 1,
+  X_ is X + 1,
+  'make-board-row'(Y, X_, Width_, Rest, This).
+
+'make-board'(Height, Width, Grid, This, This_height, This_width) :-
+  % 'make-board'/3 的帮助函数
+  % 'make-board'(+Height, +Width, -Grid, +This, +This_height, +This_width)
+  Height =:= 0, !, Grid = [];
+  Y is This_height - Height + 1,
+  X is This_width - Width + 1,
+  !, 'make-board-row'(Y, X, Width, List, This),
+  Grid = [List|Rest],
   Height_ is Height - 1,
-  'make-board'(Height_,Width,Rest).
+  'make-board'(Height_,Width, Rest, This, This_height, This_width).
+
+'make-board'(Height, Width, Grid) :-
+  % 'make-board'(+Height, +Width, -Grid)
+  % 创建出 Height 行，Width 列的网格
+  'make-board'(Height, Width, Grid, Grid, Height, Width).
 
 'get-board'(Row,Col,Board,X) :-
   % 从网格中指定索引获取元素
   nth1(Row,Board,Board_),
   nth1(Col,Board_,X).
 
-'get-3'(C_center, Row, List) :-
+'get-3'(X_center, Row, List) :-
   % 从一行中截取 3 个元素（自动处理边缘情况）
-  C_start is C_center -1,
-  C_end is C_center +1,
+  X_start is X_center -1,
+  X_end is X_center +1,
   findall(Element, 
-           (between(C_start, C_end, Index),
+           (between(X_start, X_end, Index),
             nth1(Index, Row, Element)),
           List).
 
-'get-3x3'(R_center,C_center,Board,List) :-
+'get-3x3'(Y_center,X_center,Board,List) :-
   % 从网格中截取 3x3 元素（自动处理边缘 2x2、2x3 等情况）
-  R_start is R_center -1,
-  R_end is R_center +1,
+  Y_start is Y_center -1,
+  Y_end is Y_center +1,
   findall(Element, 
-           (between(R_start, R_end, Index),
+           (between(Y_start, Y_end, Index),
             nth1(Index, Board, Element)),
           List_),
-  maplist('get-3'(C_center), List_, List__),
+  maplist('get-3'(X_center), List_, List__),
   flatten(List__,List). % 扁平化截取结果
 
 main() :-
